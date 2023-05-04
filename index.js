7389df2a72be6b3970f03fa5410620df4d46f9aa
Rodrigo Silva de Azevedo <128925998+cgfrs@users.noreply.github.com>
2023-05-04 16:37:02 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const numPerguntas = 2;
+const respostasCorretas = ['Brasília', 'Rússia'];
+const respostasSelecionadas = [];
+
+const perguntas = document.querySelectorAll('.pergunta');
+const resultado = document.getElementById('resultado');
+
+function mostrarResultado() {
+  let numAcertos = 0;
+
+  for (let i = 0; i < numPerguntas; i++) {
+    if (respostasSelecionadas[i] === respostasCorretas); 
+      
+  }
+}
